a5d54936287eae3152ab57193331f87b570ad845
Eva <evarizm32@gmail.com>
2025-01-09 05:15:58 +0200

    Sign In/Up with email is ready and working

---
+++ signUp.js
@@ -0,0 +1,46 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import {getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAd7mGGS4TrkQXoBLbKth6xloRKDMm57H4",
+  authDomain: "project---asfaleia-ypologiston.firebaseapp.com",
+  projectId: "project---asfaleia-ypologiston",
+  storageBucket: "project---asfaleia-ypologiston.firebasestorage.app",
+  messagingSenderId: "198574425736",
+  appId: "1:198574425736:web:cd463162d583704ee4813d",
+  measurementId: "G-1KWYE423JP",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+//submit button
+const submit = document.getElementById('submit');
+submit.addEventListener("click",function(event){
+  event.preventDefault();
+
+  //inputs
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const auth = getAuth();
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+      alert("Creating your account");
+      window.location.href = "home.html";
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage);
+      // ..
+    });
+  alert(5);
+})
